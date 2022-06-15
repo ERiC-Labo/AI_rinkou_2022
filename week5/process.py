40676b2706492e532822bf80624155bd0f6dbc8c
bon-taku521 <84554907+bon-taku521@users.noreply.github.com>
2022-06-15 16:29:29 +0900

    Delete week5 directory

--- week5/process.py
+++
@@ -1,7 +0,0 @@
-def process_df(df):
-    df = df.drop(["PassengerId", "Name", "Ticket", "Cabin", "Embarked"], axis=1)
-    df["Age"] = df["Age"].fillna(df["Age"].mean())
-    df = df.replace("male", 0)
-    df = df.replace("female", 1)
-    return df
-
